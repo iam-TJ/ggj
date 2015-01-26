dd86eaf66c141c7b3aa7ecb77862f415e7f481fe
TJ <ggj@iam.tj>
2015-01-26 17:56:44 +0000

    Add assemble module template

--- src/assets/script/assemble.js
+++ src/assets/script/assemble.js
@@ -3,15 +3,15 @@
 
 if (debug) console.log("Starting Assembly...");
 
+console.log('ass #1');
 var tool = new paper.Tool();
 
+console.log('ass #2');
 var ass_resource_count = 1;
 
 var crane_base = { item: undefined, active: true, uri: "assets/crane-01.svg" };
 
-function run() {
- load_resources();
-}
+load_resources();
 
 // Wait for resources to be loaded before calling init()
 function is_ready() {
@@ -19,7 +19,7 @@ function is_ready() {
   if (ass_resource_count == 0)
    init();
   else
-   setTimeout(is_Ready, 1000);
+   setTimeout(is_ready, 1000);
 }
 
 
@@ -34,13 +34,15 @@ function load_resources() {
 
   if (debug) console.log("Canvas size " + canv.width + "," + canv.height );
 
-  if (crane_base.active) request_resource_svg(crane_base, set_SVG);
+  // if (crane_base.active) request_resource_svg(crane_base, set_SVG);
   
-  is_ready();
+  // is_ready();
+  init(); // short-circuit out
 }
 
 
 
 function init() {
   if (debug) console.log("assembly init()");
+  done = true;
 }
